55d3e26c6e465c3c5ade252ce534b3374eb45a30
hbybyyang <2451759073@qq.com>
2024-09-04 06:03:17 +0800

    更新键位

--- keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/lsby/keymap.c
+++ keyboards/kprepublic/bm60hsrgb_poker/rev2/keymaps/lsby/keymap.c
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// 键位参考: https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes.md
+
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -25,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              KC_LEFT,  KC_DOWN,  KC_RIGHT,       MO(1)
     ),
     [1] = LAYOUT_60_ansi(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DELETE,
+        KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DELETE,
         _______,     RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
         _______,       _______,  _______,  _______,  _______,  _______,  KC_INSERT,  KC_HOME,  KC_PAGE_UP, _______, _______, _______, _______,
         _______,            _______, _______, _______, _______, _______, _______, KC_END, KC_PAGE_DOWN, _______, KC_SLSH, _______,
